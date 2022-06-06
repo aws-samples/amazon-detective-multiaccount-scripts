6c39789eb1e497bfe8cf306d4d45a9a25fd7c683
Yoshiyuki Kanno <kannoyos@amazon.com>
2022-06-06 02:56:56 +0000

    Fix disableDetective.py to work properly

--- disableDetective.py
+++ disableDetective.py
@@ -295,8 +295,8 @@ if __name__ == '__main__':
         logging.exception(f'error creating session {e.args}')
 
     #Chunk the list of accounts in the .csv into batches of 50 due to the API limitation of 50 accounts per invokation
-    for chunk in chunked(aws_account_dict.items(), 50):
-
+    for chunk_tuple in chunked(aws_account_dict.items(), 50):
+        chunk = {x: y for x, y in chunk_tuple}
         for region in detective_regions:
             try:
                 d_client = master_session.client('detective', region_name=region)
@@ -311,7 +311,7 @@ if __name__ == '__main__':
                         if not args.delete_graph:
                             delete_members(d_client, graph, chunk)
                         else:
-                            d_client.delete_graph(graph)
+                            d_client.delete_graph(GraphArn=graph)
                 except NameError as e:
                     logging.error(f'account is not defined: {e}')
                 except Exception as e:
